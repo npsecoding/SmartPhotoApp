cce953a6a6ed1dc30b5d3a188c11333a6a611678
Nancy Pang <nancy.pang@alumni.ubc.ca>
2016-04-03 15:47:36 -0700

    Fixed the settings overlapping toolbar problem

--- app/src/main/java/com/example/nancy/smartphoto/SettingsView.java
+++ app/src/main/java/com/example/nancy/smartphoto/SettingsView.java
@@ -2,16 +2,12 @@ package com.example.nancy.smartphoto;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class SettingsView extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsView extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +19,6 @@ public class SettingsView extends PreferenceActivity implements SharedPreference
                 .commit();
 
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
-    }
-
-    //Toolbar
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
-        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
-        root.addView(bar, 0); // insert at top
-
-        bar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
 
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
